0da00a7540ad2260faf8cd2e3db5501eaf999dee
Your Name <you@example.com>
2023-05-25 02:10:01 +0300

    push rawan files

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const router = new Router();
 
-import { home, index, login, subjectPage, addSubject, createSubject, editSubject} from "../controllers/admin.js"
+import { home, index, login, subjectPage, addSubject, createSubject, editSubject,studentPage,createstudent,departmentPage,creatdepartment} from "../controllers/admin.js"
 import subject from "../models/subject.js";
 
 router.post('/login', login)
@@ -12,8 +12,10 @@ router.get('/addSubject/:id/edit', editSubject)
 router.post('/createSubject', createSubject)
 
 router.get('/subject', subjectPage)
-
-
+router.get('/student', studentPage)
+router.post('/student', createstudent)
+router.get('/department', departmentPage)
+router.post('/department', creatdepartment)
 router.get('/', index);
 
 export default router;
