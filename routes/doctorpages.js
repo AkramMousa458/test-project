d014e7ce13256c7f00f3fc882f1bb4d2da86da1f
gehangM <123267560+gehangM@users.noreply.github.com>
2023-05-25 03:51:17 +0300

    Delete routes directory

--- routes/doctorpages.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { doctorAndroid } from "../controllers/admin";
-const router = new Router();
-
-
-
-router.get('/doctorAndroid' , doctorAndroid)
-
-export default router;
--- routes/login.js
+++
@@ -1,6 +0,0 @@
-import { login } from "../controllers/login";
-import { Router } from "express";
-const routerPost = new Router();
-routerPost.post('/login', login)
-
-export default routerPost;
