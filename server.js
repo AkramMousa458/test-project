cc3e89323fa5a3047cb7281357d155022865f132
Your Name <you@example.com>
2023-05-25 03:09:15 +0200

    ahmed negm branch

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from  "body-parser"
-
+import  methodOverride from "method-override";
 const app = express();
 
 import { engine } from "express-handlebars";
@@ -14,7 +14,7 @@ mongoose.connect(process.env.mongoConnectionUrl)
 
 import Routers from "./routes/admin.js"
 
-
+app.use(methodOverride('_method'));
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
@@ -26,7 +26,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('./public'))
 
 app.use('/home', Routers)
- 
 
 app.listen(process.env.PORT, () =>{
     console.log(`Started app on http://localhost:${process.env.PORT}/home`)
